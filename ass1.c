ad70b528eafd14d11da58dd1b0127541f94c603b
Pranav Patil <118053113+pranavpatilgithub@users.noreply.github.com>
2024-04-21 23:34:18 +0530

    Update ass1.c

--- ass1.c
+++ ass1.c
@@ -1,134 +1,76 @@
-//FCFS
-#include <iostream>
-#include <algorithm>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-using namespace std;
-
-int main() {
-    cout << "Enter the number of Processes: ";
-    int n;
-    cin >> n;
-
-    int p[n], at[n], bt[n], ct[n], tat[n], wt[n];
-    double avgTAT = 0.0, avgWT = 0.0;
-
-    for (int i = 0; i < n; i++) {
-        p[i] = i + 1; // Process numbers start from 1
-    }
-
-    for (int i = 0; i < n; i++) {
-        cout << "Enter Process " << p[i] << " Arrival Time: ";
-        cin >> at[i];
-    }
-
-    for (int i = 0; i < n; i++) {
-        cout << "Enter Process " << p[i] << " Burst Time: ";
-        cin >> bt[i];
-    }
-
-    // Sort processes based on arrival time
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (at[j] > at[j + 1]) {
-                // Swap the processes if they are out of order
-                swap(at[j], at[j + 1]);
-                swap(bt[j], bt[j + 1]);
-                swap(p[j], p[j + 1]);
-            }
-        }
-    }
-
-    // Calculate completion time, turnaround time, and waiting time
-    ct[0] = at[0] + bt[0];
-    tat[0] = ct[0] - at[0];
-    wt[0] = tat[0] - bt[0];
-
-    for (int i = 1; i < n; i++) {
-        ct[i] = ct[i - 1] + bt[i];
-        tat[i] = ct[i] - at[i];
-        wt[i] = tat[i] - bt[i];
-    }
-
-    // Calculate average turnaround time and average waiting time
-    for (int i = 0; i < n; i++) {
-        avgTAT += tat[i];
-        avgWT += wt[i];
-    }
-
-    avgTAT /= n;
-    avgWT /= n;
-
-    // Display results
-    cout << "\nProcess\tArrival Time\tBurst Time\tCompletion Time\tTurnaround Time\tWaiting Time\n";
-    for (int i = 0; i < n; i++) {
-        cout << p[i] << "\t" << at[i] << "\t\t" << bt[i] << "\t\t" << ct[i] << "\t\t" << tat[i] << "\t\t" << wt[i] << endl;
-    }
-
-    // Display average turnaround time and average waiting time
-    cout << "\nAverage Turnaround Time: " << avgTAT << endl;
-    cout << "Average Waiting Time: " << avgWT << endl;
-
-    return 0;
+void copyfile(){
+	pid_t pid = fork();
+	if(pid < 0){
+		printf("fork failed");
+	}
+	else if(pid == 0){
+		char *args[] = {"cp" , "source.txt" , "dest.txt" , NULL};
+		execvp("cp",args);
+		perror("execvp");
+		exit(EXIT_FAILURE);
+	}
+	else{
+	    wait(NULL);
+	    printf("file copied");
+	}
+	
+}
+void searchpattern(){
+	pid_t pid = fork();
+	char pattern[256];
+	printf("enter pattern to search = ");
+	scanf("%255s",pattern);
+	
+	if(pid < 0){
+		printf("fork failed");
+	}
+	else if(pid == 0){
+		char *args[] = {"grep" , pattern , "source.txt" , NULL};
+		execvp("grep",args);
+		perror("execvp");
+		exit(EXIT_FAILURE);
+	}
+	else{
+	    wait(NULL);
+	    printf("pattern searched");
+	}
+	
 }
 
-//SJF
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-int main() {
-    int n;
-    cout << "Enter no of process: ";
-    cin >> n;
-
-    vector<int> pid(n), at(n), bt(n), ct(n), ta(n), wt(n), f(n, 0);
-    int st = 0, tot = 0;
-    float avgwt = 0, avgta = 0;
-
-    for (int i = 0; i < n; i++) {
-        cout << "Enter process " << (i + 1) << " arrival time: ";
-        cin >> at[i];
-        cout << "Enter process " << (i + 1) << " burst time: ";
-        cin >> bt[i];
-        pid[i] = i + 1;
-    }
-
-    while (true) {
-        int c = n, min = 999;
-
-        if (tot == n)
-            break;
-
-        for (int i = 0; i < n; i++) {
-            if ((at[i] <= st) && (f[i] == 0) && (bt[i] < min)) {
-                min = bt[i];
-                c = i;
-            }
-        }
-
-        if (c == n)
-            st++;
-        else {
-            ct[c] = st + bt[c];
-            st += bt[c];
-            ta[c] = ct[c] - at[c];
-            wt[c] = ta[c] - bt[c];
-            f[c] = 1;
-            tot++;
-        }
-    }
-
-    cout << "\nProcess\tArrival Time\tBurst Time\tCompletion Time\tTurnaround Time\tWaiting Time\n";
-
-    for (int i = 0; i < n; i++) {
-        avgwt += wt[i];
-        avgta += ta[i];
-        cout << pid[i] << "\t" << at[i] << "\t\t" << bt[i] << "\t\t" << ct[i] << "\t\t" << ta[i] << "\t\t" << wt[i] << "\n";
-    }
-
-    cout << "\nAverage Turnaround Time: " << (avgta / n) << "\n";
-    cout << "Average Waiting Time: " << (avgwt / n) << "\n";
-
-    return 0;
+int main(){
+    int ch;
+    
+    printf("1. copy command\n");
+    printf("2. search pattern\n");
+    printf("3. Get pid\n");
+    printf("4. exit\n");
+    
+    printf("enter your choice = ");
+    scanf("%d",&ch);
+    
+    
+    
+    switch(ch){
+    	case 1:
+    		copyfile();
+    		break;
+    	case 2:
+			searchpattern();
+			break;
+		case 3:
+			printf("The child id = %d",getpid());
+			break;
+		case 4:
+			printf("= = exiting = =");
+			break;
+		default:
+			printf("enter valid number\n");		
+	}
+	return 0;
+	
 }
